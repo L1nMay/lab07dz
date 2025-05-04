4daf3c754d7ec3f5ed589eae8ef8537e1aaf014b
L1nMay <ilazayzev@gmail.com>
2025-05-04 05:16:13 -0400

    New packet and release

---
+++ .hunter/config.cmake
@@ -0,0 +1,9 @@
+cat << 'EOF' > .hunter/config.cmake
+hunter_config(
+    lab07dz
+    VERSION ${HUNTER_lab07dz_VERSION}
+    CMAKE_ARGS
+        BUILD_SHARED_LIBS=ON
+        LAB07DZ_BUILD_TESTS=ON
+)
+EOF
